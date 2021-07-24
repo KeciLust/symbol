cb385145b357c1dc164fed351902cdc025fac61a
Denis Nikitin <dinya.nikitin@gmail.com>
2021-07-24 11:49:19 +0300

    finish 0.0.1 commit

--- src/js/app.js
+++ src/js/app.js
@@ -5,15 +5,20 @@ export default class Team {
 
   // eslint-disable-next-line class-methods-use-this
   [Symbol.iterator]() {
+    const keys = Object.keys(this.persons);
+    const limit = keys.length;
+    const personFn = this.persons;
     let length = 0;
+
     return {
+
       next() {
-        if (length < this.persons.length) {
+        if (length < limit) {
           return {
             done: false,
 
             // eslint-disable-next-line no-plusplus
-            value: this.persons[length++],
+            value: personFn[length++],
           };
         }
         return {
